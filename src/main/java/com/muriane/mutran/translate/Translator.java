8952fd7bd7b4be9df6292782800233a11ce54e38
MuRiane0407 <chenjh669@mail2.sysu.edu.cn>
2026-03-25 11:21:02 +0800

    0.4.4 修复合并翻译模式发生错误不在客户端警告的问题，优化翻译请求逻辑

--- src/main/java/com/muriane/mutran/translate/Translator.java
+++ src/main/java/com/muriane/mutran/translate/Translator.java
@@ -10,11 +10,6 @@ import net.minecraft.network.chat.Component;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import oshi.util.tuples.Pair;
 
 import java.net.URI;
